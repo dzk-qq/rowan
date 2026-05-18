03a11bd6be3573f16bf1cc7113d8593a7e4749ab
DZK <contacto.dzk@gmail.com>
2026-05-17 22:32:26 -0300

    Update: 2026-05-17 22:32

--- js/static-data.js
+++ js/static-data.js
@@ -1,5 +1,5 @@
 /* AUTO-GENERADO por build.js — no editar manualmente */
-/* Build: 2026-05-18T01:30:24.053Z */
+/* Build: 2026-05-18T01:32:26.811Z */
 window.__STATIC_DATA = {
   "releases": [
     {
